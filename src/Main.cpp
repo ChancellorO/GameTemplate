4af51ebfedfc55d1aabad5212b12a560301c5114
ketexon <zane.a.s.clark@gmail.com>
2024-04-25 22:02:08 -0700

    started setting up for emscripten

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,46 +2,60 @@
 #include <box2d/box2d.h>
 #include <entt/entt.hpp>
 
+#ifdef PLATFORM_WEB
+#include <emscripten/emscripten.h>
+#endif
+
 constexpr int kWindowWidth = 500;
 constexpr int kWindowHeight = 500;
 const char* kWindowTitle = "Title";
 
 constexpr Color kBackgroundColor = Color { 255, 255, 255, 255 };
 
+static Texture cat;
+
+void MainLoop(){
+	BeginDrawing();
+
+	ClearBackground(kBackgroundColor);
+
+	DrawTexturePro(
+		cat,
+		// source
+		Rectangle {
+			0, 0,
+			static_cast<float>(cat.width), static_cast<float>(cat.height),
+		},
+		// dest
+		Rectangle {
+			24, 24,
+			static_cast<float>(GetScreenWidth() - 48), static_cast<float>(GetScreenHeight() - 48),
+		},
+		// origin
+		Vector2 { 0, 0 },
+		// rotation
+		0,
+		// tint
+		RAYWHITE
+	);
+
+	EndDrawing();
+}
+
 int main(){
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
 	InitWindow(kWindowWidth, kWindowHeight, kWindowTitle);
 
-	const auto cat = LoadTexture("resources/cat.jpg");
+	cat = LoadTexture("resources/cat.jpg");
 
+#ifdef PLATFORM_WEB
+	emscripten_set_main_loop(MainLoop, 0, 1);
+#else
 	while(!WindowShouldClose()){
-		BeginDrawing();
-
-		ClearBackground(kBackgroundColor);
-
-		DrawTexturePro(
-			cat,
-			// source
-			Rectangle {
-				0, 0,
-				static_cast<float>(cat.width), static_cast<float>(cat.height),
-			},
-			// dest
-			Rectangle {
-				24, 24,
-				static_cast<float>(GetScreenWidth() - 48), static_cast<float>(GetScreenHeight() - 48),
-			},
-			// origin
-			Vector2 { 0, 0 },
-			// rotation
-			0,
-			// tint
-			RAYWHITE
-		);
-
-		EndDrawing();
+		MainLoop();
 	}
+#endif
 
 	CloseWindow();
 
